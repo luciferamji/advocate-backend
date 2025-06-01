20b50692ead1214c5c4016a60ad3628fdb0b4ff5
Akshat Jaitly <38823529+luciferamji@users.noreply.github.com>
2025-06-01 18:17:39 +0530

    Update Case Comments System

--- models/caseComment.model.js
+++ models/caseComment.model.js
@@ -19,11 +19,27 @@ module.exports = (sequelize, DataTypes) => {
     },
     createdBy: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: true, // Changed to allow null for client comments
       references: {
         model: 'admin',
         key: 'id'
       }
+    },
+    // New fields for client comments
+    clientName: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    clientEmail: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    clientPhone: {
+      type: DataTypes.STRING,
+      allowNull: true
     }
   }, {
     timestamps: true,
