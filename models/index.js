ae079ebad97fac4d2109d3fbbde4597c3732c5c7
mishratharva <mishratharva@gmail.com>
2025-06-03 20:08:44 +0530

    updating index.js

--- models/index.js
+++ models/index.js
@@ -1,30 +1,162 @@
-// Add to existing associations
-db.DocumentLink = require('./documentLink.model')(sequelize, Sequelize);
+const { Sequelize } = require('sequelize');
+const dbConfig = require('../config/db.config');
 
-// Add these associations
-db.Case.hasMany(db.DocumentLink, {
-  foreignKey: 'caseId',
-  as: 'documentLinks'
+// Create Sequelize instance
+const sequelize = new Sequelize(
+  dbConfig.DB,
+  dbConfig.USER,
+  dbConfig.PASSWORD,
+  {
+    host: dbConfig.HOST,
+    port: dbConfig.PORT,
+    dialect: dbConfig.dialect,
+    pool: dbConfig.pool,
+    logging: process.env.NODE_ENV === 'development' ? console.log : false
+  }
+);
+
+// Initialize db object
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+// Import models
+db.Admin = require('./admin.model')(sequelize, Sequelize);
+db.Advocate = require('./advocate.model')(sequelize, Sequelize);
+db.Client = require('./client.model')(sequelize, Sequelize);
+db.Case = require('./case.model')(sequelize, Sequelize);
+db.Hearing = require('./hearing.model')(sequelize, Sequelize);
+db.HearingComment = require('./hearingComment.model')(sequelize, Sequelize);
+db.HearingCommentDoc = require('./hearingCommentDoc.model')(sequelize, Sequelize);
+db.CaseComment = require('./caseComment.model')(sequelize, Sequelize);
+db.CaseCommentDoc = require('./caseCommentDoc.model')(sequelize, Sequelize);
+db.UploadLink = require('./uploadLink.model')(sequelize, Sequelize);
+
+// Define associations
+
+// Admin - Advocate relationship (1:1)
+db.Admin.hasOne(db.Advocate);
+db.Advocate.belongsTo(db.Admin);
+
+// Admin - Client relationship (1:Many)
+db.Admin.hasMany(db.Client, {
+  foreignKey: 'createdBy',
+  as: 'clients'
 });
-db.DocumentLink.belongsTo(db.Case, {
+db.Client.belongsTo(db.Admin, {
+  foreignKey: 'createdBy',
+  as: 'admin'
+});
+
+// Client - Case relationship (1:Many)
+db.Client.hasMany(db.Case, {
+  foreignKey: 'clientId',
+  as: 'cases'
+});
+db.Case.belongsTo(db.Client, {
+  foreignKey: 'clientId',
+  as: 'client'
+});
+
+// Admin - Case relationship (1:Many)
+db.Admin.hasMany(db.Case, {
+  foreignKey: 'advocateId',
+  as: 'cases'
+});
+db.Case.belongsTo(db.Admin, {
+  foreignKey: 'advocateId',
+  as: 'advocate'
+});
+
+// Case - Hearing relationship (1:Many)
+db.Case.hasMany(db.Hearing, {
+  foreignKey: 'caseId',
+  as: 'hearings'
+});
+db.Hearing.belongsTo(db.Case, {
   foreignKey: 'caseId',
   as: 'case'
 });
 
-db.Hearing.hasMany(db.DocumentLink, {
+// Hearing - HearingComment relationship (1:Many)
+db.Hearing.hasMany(db.HearingComment, {
   foreignKey: 'hearingId',
-  as: 'documentLinks'
+  as: 'comments'
 });
-db.DocumentLink.belongsTo(db.Hearing, {
+db.HearingComment.belongsTo(db.Hearing, {
   foreignKey: 'hearingId',
   as: 'hearing'
 });
 
-db.Admin.hasMany(db.DocumentLink, {
+// Admin - HearingComment relationship (1:Many)
+db.Admin.hasMany(db.HearingComment, {
   foreignKey: 'createdBy',
-  as: 'documentLinks'
+  as: 'hearingComments'
 });
-db.DocumentLink.belongsTo(db.Admin, {
+db.HearingComment.belongsTo(db.Admin, {
   foreignKey: 'createdBy',
-  as: 'creator'
+  as: 'user'
 });
+
+// HearingComment - HearingCommentDoc relationship (1:Many)
+db.HearingComment.hasMany(db.HearingCommentDoc, {
+  foreignKey: 'hearingCommentId',
+  as: 'documents'
+});
+db.HearingCommentDoc.belongsTo(db.HearingComment, {
+  foreignKey: 'hearingCommentId',
+  as: 'comment'
+});
+
+// Case - CaseComment relationship (1:Many)
+db.Case.hasMany(db.CaseComment, {
+  foreignKey: 'caseId',
+  as: 'comments'
+});
+db.CaseComment.belongsTo(db.Case, {
+  foreignKey: 'caseId',
+  as: 'case'
+});
+
+// Admin - CaseComment relationship (1:Many)
+db.Admin.hasMany(db.CaseComment, {
+  foreignKey: 'createdBy',
+  as: 'caseComments'
+});
+db.CaseComment.belongsTo(db.Admin, {
+  foreignKey: 'createdBy',
+  as: 'user'
+});
+
+// CaseComment - CaseCommentDoc relationship (1:Many)
+db.CaseComment.hasMany(db.CaseCommentDoc, {
+  foreignKey: 'caseCommentId',
+  as: 'documents'
+});
+db.CaseCommentDoc.belongsTo(db.CaseComment, {
+  foreignKey: 'caseCommentId',
+  as: 'comment'
+});
+
+// Case - UploadLink relationship (1:Many)
+db.Case.hasMany(db.UploadLink, {
+  foreignKey: 'caseId',
+  as: 'uploadLinks'
+});
+db.UploadLink.belongsTo(db.Case, {
+  foreignKey: 'caseId',
+  as: 'case'
+});
+
+// Admin - UploadLink relationship (1:Many)
+db.Admin.hasMany(db.UploadLink, {
+  foreignKey: 'createdBy',
+  as: 'uploadLinks'
+});
+db.UploadLink.belongsTo(db.Admin, {
+  foreignKey: 'createdBy',
+  as: 'admin'
+});
+
+module.exports = db;
