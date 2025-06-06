57a2ffab233cc9f6252edacbc5de57be6ed30c14
mishratharva <mishratharva@gmail.com>
2025-06-06 14:34:51 +0530

    added functions for invoice controler

--- routes/invoice.routes.js
+++ routes/invoice.routes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const {
   generateInvoice,
-  getInvoices
+  getInvoices,
+  getInvoice,
+  updateInvoice,
+  deleteInvoice
 } = require('../controllers/invoice.controller');
 const { protect } = require('../middleware/auth.middleware');
 
@@ -10,5 +13,13 @@ const router = express.Router();
 // Protected routes
 router.route('/generate')
   .post(protect, generateInvoice);
+
+router.route('/')
+  .get(protect, getInvoices);
+
+router.route('/:id')
+  .get(protect, getInvoice)
+  .put(protect, updateInvoice)
+  .delete(protect, deleteInvoice);
 
 module.exports = router;
