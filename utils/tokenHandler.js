bb5b632701bacf60824f936a620fb412e220edff
luciferamji <akshatjaitly@gmail.com>
2025-06-09 02:32:05 +0530

    cookie fix 2

--- utils/tokenHandler.js
+++ utils/tokenHandler.js
@@ -33,7 +33,7 @@ const sendTokenResponse = async (user, statusCode, res) => {
   const options = {
     expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // 24 hours
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
+    secure: false,
     sameSite: 'strict'
   };
 
