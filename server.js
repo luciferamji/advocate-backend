9141aa5045b509e28685936ffe8c0983aa03eaa6
luciferamji <akshatjaitly@gmail.com>
2025-06-09 02:09:24 +0530

    server test push

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.NODE_ENV === 'prod' 
-    ? process.env.FRONTEND_URL 
-    : 'http://localhost:5173',
+  // origin: process.env.NODE_ENV === 'production' 
+  //   ? process.env.FRONTEND_URL 
+  //   : 'http://localhost:5173',
   credentials: true,
 }));
 app.use(express.json());
