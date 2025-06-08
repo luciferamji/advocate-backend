b56e1b0cc050c3dab49341ed85635cd43fd69b0f
luciferamji <akshatjaitly@gmail.com>
2025-06-09 02:23:41 +0530

    test cors

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  // origin: process.env.NODE_ENV === 'production' 
-  //   ? process.env.FRONTEND_URL 
-  //   : 'http://localhost:5173',
+  origin: process.env.NODE_ENV === 'production' 
+    ? 'http://150.241.247.100:5000' // Replace with your production frontend URL
+    : 'http://localhost:5173',
   credentials: true,
 }));
 app.use(express.json());
