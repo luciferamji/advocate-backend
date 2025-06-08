770861f15afa716828de81b641301baf874e7fba
luciferamji <akshatjaitly@gmail.com>
2025-06-08 18:07:24 +0530

    updated  code

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' 
+  origin: process.env.NODE_ENV === 'prod' 
     ? process.env.FRONTEND_URL 
     : 'http://localhost:5173',
   credentials: true,
