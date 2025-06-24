7c26c91ad9d9c474b1e85dfdd8cfa406a22f791d
luciferamji <akshatjaitly@gmail.com>
2025-06-25 00:46:06 +0530

    updated name dns

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? 'https://lawfypro.lawfyco.com/' 
+    ? 'https://pro.lawfyco.com/' 
     : 'http://localhost:5173',
   credentials: true,
 }));
